c8d5bbdfd416d315dfe2ef831bfbcd897cb1b3e6
MacBook Pro <macbookpro@192.168.1.6>
2026-07-07 05:07:58 +0700

    Fix JS error after removing dark mode

--- public/frontend/assets/js/main.js
+++ public/frontend/assets/js/main.js
@@ -115,36 +115,36 @@ const scrollUp = () => {
 };
 window.addEventListener("scroll", scrollUp);
 
-/*=============== DARK LIGHT THEME ===============*/
-const themeButton = document.getElementById("theme-button");
-const darkTheme = "dark-theme";
-const iconTheme = "bx-sun";
+// /*=============== DARK LIGHT THEME ===============*/
+// const themeButton = document.getElementById("theme-button");
+// const darkTheme = "dark-theme";
+// const iconTheme = "bx-sun";
 
-if (themeButton) {
-    // Previously selected topic (if user selected)
-    const selectedTheme = localStorage.getItem("selected-theme");
-    const selectedIcon = localStorage.getItem("selected-icon");
+// if (themeButton) {
+//     // Previously selected topic (if user selected)
+//     const selectedTheme = localStorage.getItem("selected-theme");
+//     const selectedIcon = localStorage.getItem("selected-icon");
 
-    // We obtain the current theme that the interface has by validating the dark-theme class
-    const getCurrentTheme = () =>
-        document.body.classList.contains(darkTheme) ? "dark" : "light";
-    const getCurrentIcon = () =>
-        themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun";
+//     // We obtain the current theme that the interface has by validating the dark-theme class
+//     const getCurrentTheme = () =>
+//         document.body.classList.contains(darkTheme) ? "dark" : "light";
+//     const getCurrentIcon = () =>
+//         themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun";
 
-    // We validate if the user previously chose a topic
-    if (selectedTheme) {
-        document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
-        themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](iconTheme);
-    }
+//     // We validate if the user previously chose a topic
+//     if (selectedTheme) {
+//         document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
+//         themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"](iconTheme);
+//     }
 
-    // Activate / deactivate the theme manually with the button
-    themeButton.addEventListener("click", () => {
-        document.body.classList.toggle(darkTheme);
-        themeButton.classList.toggle(iconTheme);
-        localStorage.setItem("selected-theme", getCurrentTheme());
-        localStorage.setItem("selected-icon", getCurrentIcon());
-    });
-}
+//     // Activate / deactivate the theme manually with the button
+//     themeButton.addEventListener("click", () => {
+//         document.body.classList.toggle(darkTheme);
+//         themeButton.classList.toggle(iconTheme);
+//         localStorage.setItem("selected-theme", getCurrentTheme());
+//         localStorage.setItem("selected-icon", getCurrentIcon());
+//     });
+// }
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
