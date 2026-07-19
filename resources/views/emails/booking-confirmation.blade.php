2e9972aac42e72b18b26ee4fe42dd6cd77d669b4
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2026-07-19 13:27:50 +0700

    Fix: correct order of travel_package relation check in email template

--- resources/views/emails/booking-confirmation.blade.php
+++ resources/views/emails/booking-confirmation.blade.php
@@ -144,10 +144,10 @@
             <div class="detail-row">
                 <span class="detail-label">{{ __('Package') }}</span>
                 <span class="detail-value">
-                    @if($booking->travelPackage)
-                        <span class="package-badge">{{ $booking->travelPackage->type }}</span>
-                    @elseif($booking->travel_package)
+                    @if($booking->travel_package)
                         <span class="package-badge">{{ $booking->travel_package->type }}</span>
+                    @elseif($booking->travelPackage)
+                        <span class="package-badge">{{ $booking->travelPackage->type }}</span>
                     @else
                         -
                     @endif
